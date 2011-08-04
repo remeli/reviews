a49c099e3c163119d92d8c942cfca78b6bce2d8c
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-04 21:23:00 +0600

    CitiesController edit

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,31 +1,43 @@
 class CitiesController < ApplicationController
   
   def index
-    
+    @cities = City.all
   end
   
   def show
-    
+    @city = City.find(params[:id])
   end
   
   def new
-    
+    @city = City.new
   end
   
   def create
-    
+    @city = City.new(params[:city])
+    if @city.save
+      redirect_to(@city, :notice => "Город успешно добавлен")
+    else
+      render(:action => "new")
+    end
   end
   
   def edit
-    
+    @city = City.find(params[:id])
   end
   
   def update
-    
+    @city = City.find(params[:id])
+    if @city.update_attributes(params[:city])
+      redirect_to(@city, :notice => "Город успешно обновлен")
+    else
+      render(:action => "edit")
+    end
   end
   
   def destroy
-    
+    @city = City.find(params[:id])
+    @city.destroy
+    redirect_to(cities_url)
   end
   
 end
