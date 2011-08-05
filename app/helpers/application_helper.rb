bfbb98068f22497a8b98cffd47df1c310208e9e2
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-05 23:52:40 +0600

    css/notice shared/notice method/edit show#city

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
+  # output error messages
   def error_messages_for(object)
     render(:partial => "shared/error_messages", :locals => {:object => object})
+  end
+  
+  # flash messages
+  def flash_notice(object)
+    render(:partial => "shared/notice", :locals => { :notice => object })
   end
 end
