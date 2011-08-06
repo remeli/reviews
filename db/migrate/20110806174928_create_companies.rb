62a0f96287ad8fea390b0bfcb724c51b0922d8cc
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-07 01:47:05 +0600

    delete migration

--- db/migrate/20110806174928_create_companies.rb
+++ db/migrate/20110806174928_create_companies.rb
@@ -1,13 +1,7 @@
 class CreateCompanies < ActiveRecord::Migration
   def self.up
     create_table :companies do |t|
-      t.string :name
-      t.string :permalink
-      t.integer :rating
-      t.integer :city_id
-      t.ineteger :category_id
-      t.text :description
-      t.integer :user_id 
+
       t.timestamps
     end
   end
