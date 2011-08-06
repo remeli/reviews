cdb22f9447d038d590f872dbce6372f978c01ab6
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-07 02:24:16 +0600

    add data to seeds.rb

--- db/migrate/20110806174928_create_companies.rb
+++ db/migrate/20110806174928_create_companies.rb
@@ -4,14 +4,13 @@ class CreateCompanies < ActiveRecord::Migration
       t.string :name, :null => false, :limit => 150
       t.string :permalink, :null => false, :limit => 60
       t.integer :rating, :default => 0
-      t.text :description, :null => true
+      t.text :description
       t.integer :category_id, :null => false
       t.integer :city_id, :null => false
       t.integer :user_id, :null => false
       t.timestamps
     end
     add_index("companies","name")
-    add_index("companies", "description")
     add_index("companies", "category_id")
     add_index("companies", "city_id")
     
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
+#cities
 cities = City.create(
   [
     {:name => "Алматы", :permalink => "almaty"},
@@ -19,5 +20,19 @@ cities = City.create(
     {:name => "Уральск", :permalink => "uralsk"},
     {:name => "Караганда", :permalink => "karagandy"},
     {:name => "Тараз", :permalink => "taraz"},
+  ]
+)
+
+#companies
+companies = Company.create(
+  [
+    {:name => "Воздух", :permalink => "vozdux", :rating => 10, :description => "Клуб",
+     :category_id => 1, :city_id => 1, :user_id => 1},
+    {:name => "Малышок", :permalink => "malishok", :rating => 0, :description => "Кафе",
+     :category_id => 1, :city_id => 2, :user_id => 1},     
+    {:name => "Торт", :permalink => "tort", :rating => 10, :description => "кондитерская",
+     :category_id => 1, :city_id => 3, :user_id => 1},
+    {:name => "Школа 3", :permalink => "school3", :rating => 10, :description => "Школа",
+     :category_id => 1, :city_id => 4, :user_id => 1},
   ]
 )
