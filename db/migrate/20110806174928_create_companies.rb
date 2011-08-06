5a66e528a8e27ec4d413f9550d18649c5511cdf7
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-07 02:07:18 +0600

    change migratio

--- db/migrate/20110806174928_create_companies.rb
+++ db/migrate/20110806174928_create_companies.rb
@@ -1,8 +1,8 @@
 class CreateCompanies < ActiveRecord::Migration
   def self.up
     create_table :companies do |t|
-      t.string :name, :null => false
-      t.string :permalink, :null => false
+      t.string :name, :null => false, :limit => 150
+      t.string :permalink, :null => false, :limit => 60
       t.integer :rating, :default => 0
       t.text :description, :null => true
       t.integer :category_id, :null => false
@@ -10,6 +10,11 @@ class CreateCompanies < ActiveRecord::Migration
       t.integer :user_id, :null => false
       t.timestamps
     end
+    add_index("companies","name")
+    add_index("companies", "description")
+    add_index("companies", "category_id")
+    add_index("companies", "city_id")
+    
   end
 
   def self.down
