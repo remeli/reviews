0e90d640b09b5d6e87be42472830f691b5dea12d
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-07 01:56:26 +0600

    add change to migration

--- db/migrate/20110806174928_create_companies.rb
+++ db/migrate/20110806174928_create_companies.rb
@@ -1,7 +1,13 @@
 class CreateCompanies < ActiveRecord::Migration
   def self.up
     create_table :companies do |t|
-
+      t.string :name, :null => false
+      t.string :permalink, :null => false
+      t.integer :rating, :default => 0
+      t.text :description, :null => true
+      t.integer :category_id, :null => false
+      t.integer :city_id, :null => false
+      t.integer :user_id, :null => false
       t.timestamps
     end
   end
