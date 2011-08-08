5614b70ea2e5dc0f697f29b221a66d8acaf61a2b
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-08 14:46:48 +0600

    edit menu, add load_menu_cities method, etc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :load_cities_menu
+  
+  def load_cities_menu
+    @cities = City.limit(10)
+  end
+  
+  
 end
