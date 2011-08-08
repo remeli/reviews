1a5d9797fddfe18786cc1426f0e1366e79066911
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-08 14:47:35 +0600

    rename variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_filter :load_cities_menu
   
   def load_cities_menu
-    @cities = City.limit(10)
+    @cities_menu = City.limit(10)
   end
   
   
