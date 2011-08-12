9cda4ec128978c7835bc91c9538c1d10952c5196
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-13 02:39:34 +0600

    change migrations(category)

--- db/migrate/20110811192401_create_categories.rb
+++ db/migrate/20110811192401_create_categories.rb
@@ -1,7 +1,8 @@
 class CreateCategories < ActiveRecord::Migration
   def self.up
     create_table :categories do |t|
-
+      t.string :name, :null => false, :limit => 100
+      t.string :permalink, :null => false, :limit => 150
       t.timestamps
     end
   end
