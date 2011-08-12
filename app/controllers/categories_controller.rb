d8979f322c77f8ef36e333a68b282b7237766b85
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-13 03:02:02 +0600

    fill controller#category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,43 @@
 class CategoriesController < ApplicationController
-  def index
+  
+  
+  def show
+    @category = Category.find_by_permalink(params[:id])
+    @title = @category.name
   end
+  
+  def new
+    @category = Category.new
+    @title = "Новая категория"
+  end
+  
+  def create
+    @category = Category.new(params[:category])
+    if @category.save
+      redirect_to(@category, :notice => "Категория успешно создана")
+    else
+      render(:action => "new")
+    end
+  end
+  
+  def edit
+    @category = Category.find_by_permalink(params[:id])
+    @title = "Редактирование категории: #{@category.name}"
+  end
+  
+  def update
+    @category = Category.find_by_permalink(params[:id])
+    if @category.update_attributes(params[:category])
+      redirect_to(@category, :notice => "Категория успешно отредактирована")
+    else
+      render(:action => "edit")
+    end
+  end
+  
+  def destroy
+    @category = Category.find_by_permalink(params[:id])
+    @category.destroy
+    redirect_to(categories_url)
+  end 
 
 end
