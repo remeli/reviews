2807a2563bee6a722d99642ddbe6fdbaa16acd05
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-13 02:50:40 +0600

    fill model category(validation, scope etc)

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,17 @@
 class Category < ActiveRecord::Base
   has_many :companies
   
+  validates :name, :presence => { :message => "Пустое имя" }
+  validates :permalink, :presence => { :message => "Пустая прямая ссылка" }
+  validates :name, :uniqueness => { :message => "Такое имя уже существует" }
+  validates :permalink, :uniqueness => { :message => "Такая ссылка уже существует" }
+  validates :name , :length => { :maximum => 100 }
+  validates :permalink , :length => { :maximum => 150 }
+  
+  default_scope order("categories.name ASC")
+  
+  def to_param
+    permalink
+  end
   
 end
