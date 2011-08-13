0625ef07f63bdb66894373bf6ef60cfcd593d26c
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-13 22:56:30 +0600

    load_categories

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
+  
+  before_filter :load_categories
+  
   def index
     @title = "Компании, отзывы"
+  end
+  
+  def load_categories
+    @categories = Category.all
   end
 end
