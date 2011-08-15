e0bbc39f31b537a8155694d5c19ce8b75b3d4f8c
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-16 00:06:01 +0600

    change migrations

--- db/migrate/20110815173753_create_users.rb
+++ db/migrate/20110815173753_create_users.rb
@@ -1,12 +1,23 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-
+      t.string :email, :null => false, :limit => 100
+      t.integer :rating, :default => 0
+      t.string :first_name, :null => false, :limit => 150
+      t.string :last_name,  :null => false, :limit => 150
+      t.string :avatar_url, :default => "avatar.jpg"
+      t.text   :about, :limit => 250
+      t.boolean :admin, :default => false
+      t.integer :city_id, :null => false
+      t.string :hashed_password
+      t.string :salt
       t.timestamps
     end
+    add_index("users", "email")
   end
 
   def self.down
+    remove_index("users", "email")
     drop_table :users
   end
 end
