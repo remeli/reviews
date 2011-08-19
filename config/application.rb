8ee666d5cef4872f6b138865913e9ad61b0ddec5
Kusainov Yerlan <lagox.mail@gmail.com>
2011-08-19 18:58:34 +0600

    edit application.rb

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,6 @@ module Reviews
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    config.i18n.default_locale = :ru
   end
 end
