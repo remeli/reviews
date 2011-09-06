5c4615cf3aac33fd2cffe9db7e9e8e40db5a13a5
Kusainov Yerlan <lagox.mail@gmail.com>
2011-09-06 12:50:43 +0600

    rename type to positive(column in Reviews)

--- db/migrate/20110906054711_create_reviews.rb
+++ db/migrate/20110906054711_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
       t.text :post, :null => false
-      t.boolean :type, :default => true
+      t.boolean :positive, :default => true
       t.integer :rating, :default => 0
       t.integer :user_id, :null => false
       t.integer :company_id, :null => false
