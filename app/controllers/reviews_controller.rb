00bc6521598a21d58dd07fa1612b1e7f37d37afc
Kusainov Yerlan <lagox.mail@gmail.com>
2011-09-06 12:34:50 +0600

    fill Reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,22 @@
 class ReviewsController < ApplicationController
+
+  def index
+    @reviews = Review.all
+  end
+    
+  def new
+    @review = Review.new
+  end
+  
+  def create
+    @review = Review.new(params[:review])
+  end
+  
+  def destroy
+    @review = Review.find(params[:id])
+    @company = Company.find(@review.company_id)
+    @review.destroy
+    redirect_to(@company, :notice => "Комментарий удален")
+  end
+  
 end
