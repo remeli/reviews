42563d44ffb7416f9a07740d1c85042f8d0a381b
Kusainov Yerlan <lagox.mail@gmail.com>
2011-09-06 11:54:03 +0600

    change migration for review model

--- db/migrate/20110906054711_create_reviews.rb
+++ db/migrate/20110906054711_create_reviews.rb
@@ -1,7 +1,11 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-
+      t.text :post, :null => false
+      t.boolean :type, :default => true
+      t.integer :rating, :default => 0
+      t.integer :user_id, :null => false
+      t.integer :company_id, :null => false
       t.timestamps
     end
   end
