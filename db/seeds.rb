bbe935841079c966283fdb2c5e0a6936fa02cf67
Kusainov Yerlan <lagox.mail@gmail.com>
2011-09-06 12:46:07 +0600

    edit seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,3 +50,15 @@ companies = Company.create(
   ]
 )
 
+#reviews
+reviews = Review.create(
+  [
+    {:post => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", :type => true, :rating => 0, :user_id => 1, :company_id => 3}
+  ]
+)
+#users
+users = User.create(
+  [
+    {:email => "lagox@mail.ru", :password => "secret", :password_cofirmation => "secret", :city_id => 1, :rating => 0, :name => "Ерлан", :about => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." }
+  ]
+)
