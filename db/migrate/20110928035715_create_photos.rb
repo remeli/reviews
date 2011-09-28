89ef5ffc998f402a9e8b4d1d0736aea2b991bb8d
Kusainov Yerlan <lagox.mail@gmail.com>
2011-09-28 10:00:40 +0600

    edit migrations

--- db/migrate/20110928035715_create_photos.rb
+++ db/migrate/20110928035715_create_photos.rb
@@ -1,7 +1,8 @@
 class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
-
+      t.integer :company_id
+      t.integer :user_id
       t.timestamps
     end
   end
