0e05ed63686d1b93a2d94aa567c20593b9d844bd
Kusainov Yerlan <lagox.mail@gmail.com>
2011-10-05 10:18:59 +0600

    change db migrate files

--- db/migrate/20111004164240_create_assets.rb
+++
@@ -1,9 +0,0 @@
-class CreateAssets < ActiveRecord::Migration
-  def change
-    create_table :assets do |t|
-      t.references :photo, :polymorphic => true
-      t.timestamps
-    end
-    add_index :assets, [:photo_id, :photo_type]
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111005032816) do
-
-  create_table "assets", :force => true do |t|
-    t.integer  "photo_id"
-    t.string   "photo_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-  end
-
-  add_index "assets", ["photo_id", "photo_type"], :name => "index_assets_on_photo_id_and_photo_type"
+ActiveRecord::Schema.define(:version => 20110928035715) do
 
   create_table "categories", :force => true do |t|
     t.string   "name",       :limit => 100, :null => false
