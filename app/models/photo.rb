52079ce61c9d8a14af7fb3df6a02034ee2042ae7
Kusainov Yerlan <lagox.mail@gmail.com>
2011-10-05 16:48:57 +0600

    paperclip generate

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,14 +3,6 @@ class Photo < ActiveRecord::Base
   # associations
   belongs_to :user
   belongs_to :company
-  has_many :assets, :dependent => :destroy
+
   
-  validate :validate_attachments
-    
-  def validate_attachments
-    errors.add_to_base("Количество фотографий не должно превышать: #{Asset::Max_Attachments}") if assets.length > Asset::Max_Attachments
-    assets.each do |a|
-      errors.add_to_base("#{a.name} превышает размер #{Asset::Max_Attachments_Size/1.megabyte} mb") if a.file_size > Max_Attachments_Size
-    end
-  end
 end
